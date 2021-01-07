aa7939d32ef084d0077853b6e2cc7d300a022937
matsushinDB11 <matsushin35@gmail.com>
2021-01-07 16:49:54 +0900

    Q3

---
+++ charenge14_2.py
@@ -0,0 +1,10 @@
+def two_parameter(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(two_parameter(2, 2))
+    print(two_parameter(2, 3))
