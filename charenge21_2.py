a7ad2b0c3179c7cc19e3edeb45a1a771337dca9a
matsushinDB11 <matsushin35@gmail.com>
2021-01-08 22:16:42 +0900

    fin 21_2

--- charenge21_2.py
+++ charenge21_2.py
@@ -1,3 +1,18 @@
 import charenge21_1
-list = [1, 2, 3, 4, 5]
-stack = Stack()
+
+
+def reverse_array(x):
+    stack = charenge21_1.Stack()
+    for num in x:
+        stack.push(num)
+    list_num = len(stack.items)
+    re_list = []
+    for i in range(list_num):
+        re_list.append(stack.pop())
+    return re_list
+
+
+if __name__ == '__main__':
+    list_1 = [1, 2, 3, 4, 5]
+    list_2 = reverse_array(list_1)
+    print(list_2)
